792433242454b85b385cac8f1b227fbdce4681d2
cablehead0x194 <caleb.b.bishop@gmail.com>
2025-11-20 13:11:29 -0600

    New class
    Added class 'graphOptions' for creating graph/chart preferences. Need to
    read API more.

--- src/org/jfree/starter/Chart/graphOptions.java
+++ src/org/jfree/starter/Chart/graphOptions.java
@@ -1,6 +1,66 @@
 package org.jfree.starter.Chart;
+
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartUtils;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.ValueMarker;
+import org.jfree.chart.renderer.category.LineAndShapeRenderer;
+import org.jfree.chart.title.TextTitle;
+import org.jfree.chart.ui.HorizontalAlignment;
+import org.jfree.chart.ui.RectangleEdge;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.util.Scanner;
+
 //Create options for graph according to JChart settings available
 //want lines showing interval min and max, etc, add labels, etc.
 public class graphOptions {
+
+        /*
+        give options for type of chart.
+        e.g. Area chart, barchart, gantt, histogram, line chart, pie chart,
+
+        */
+
+    //createBarChart(String title,
+    // String categoryAxisLabel, - label for axis value
+    // String valueAxisLabel, - label for axis value
+    // CategoryDataset dataset, - data set for chart
+    // PlotOrientation orientation, -> horizontal or vertice
+    // boolean legend,-> is legend required
+        void selectionBar () {
+            String axisY;
+            String axisX;
+            String dataSetName;
+
+            Scanner input = new Scanner (System.in);
+            System.out.println("Name for category (Y) axis: ");
+            axisY = input.nextLine();
+
+            System.out.println("Name for category (X) axis: ");
+            axisX = input.nextLine();
+
+            System.out.println("Provide data set name: ");
+            dataSetName = input.nextLine();
+
+            SaveLoadDataSets sl = new SaveLoadDataSets();
+            dataPoints list = sl.ReadFile();
+
+
+        }
+
+
+
+
+
+
 
 }
