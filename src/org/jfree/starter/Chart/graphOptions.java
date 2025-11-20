70a5ab8580a86b02b143d10b1b802427d4bbefc8
cablehead0x194 <caleb.b.bishop@gmail.com>
2025-11-19 21:44:17 -0600

    Data Visualization Project New repository for learning purposes. Reasons: 1. project did not have JFreeChart library correctly added. 2. Learned to set up project as Maven. 3. Added JFreeChart Library to project.

---
+++ src/org/jfree/starter/Chart/graphOptions.java
@@ -0,0 +1,6 @@
+package org.jfree.starter.Chart;
+//Create options for graph according to JChart settings available
+//want lines showing interval min and max, etc, add labels, etc.
+public class graphOptions {
+
+}
---
+++ src/org/jfree/starter/Chart/main.java
@@ -0,0 +1,175 @@
+package org.jfree.starter.Chart;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class main {
+    public static dataPoints createDataSet() {
+        Scanner scanner = new Scanner(System.in);
+        String name ="";
+        String date = "";
+        int length = 0;
+        boolean valid = false;
+
+        System.out.println("Enter name of data elements: ");
+        name = scanner.next();
+        scanner.nextLine();
+
+        System.out.println("Enter number of data points: ");
+        do {
+            try {
+                length = scanner.nextInt();
+                scanner.nextLine();
+                valid = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a whole number.");
+                scanner.nextLine();
+            }
+        }   while (!valid);
+
+
+        dataPoints array = new dataPoints(name, length);
+        return array;
+    }
+
+    /**
+     * Method to add data points to existing array
+     * no return needed.
+     * Need to make the array accesible but immutable
+     */
+    public static void addDataPoints(dataPoints array) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            dataPoints list = array;
+            double points = 0.0;
+            String date = "";
+
+            //need to add exception for wrong input type and try again message.
+            for (int i = 0; i < array.getArray().length; ++i) {
+                System.out.println("Enter data point: ");
+                points = scanner.nextDouble();
+                scanner.nextLine();
+
+                System.out.println("Enter date (DD/MM/YYYY): ");
+                date = scanner.next();
+
+                array.addElement(points, date);
+            }
+        }
+        catch (NullPointerException e) {
+            System.out.println("Data set has not been initialized or loaded.");
+        }
+    }
+
+    //to view data set
+    public static void printDataPoints (dataPoints list) {
+        try {
+            list.printArray();
+        }
+        catch (NullPointerException e) {
+            System.out.println("A data set does not exist to save.");
+        }
+    }
+
+    public static void SaveObjectArrayStart(dataPoints list) {
+        SaveLoadDataSets sl = new SaveLoadDataSets(list);
+        sl.SaveArray();
+    }
+
+    public static dataPoints loadDataSet() {
+        SaveLoadDataSets sl = new SaveLoadDataSets();
+        return sl.ReadFile();
+    }
+
+    public static void loadSets() {
+        SaveLoadDataSets ls =  new SaveLoadDataSets();
+        ls.viewSavedDataSets();
+    }
+
+    public static void grOptionsMenu() {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("\n=== Graph Options Menu ===");
+            System.out.println("1. Create Data Set");
+
+            System.out.println("0. Return to main menu.");
+            System.out.print("Enter your choice: ");
+
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Doesn't do anything yet");
+
+                case 0:
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+        while (choice != 0) ;
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+
+        dataPoints[] list = new dataPoints[10];
+
+        //create menu to select
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("\n=== MAIN MENU ===");
+            System.out.println("1. Create Data Set");
+            System.out.println("2. View Data Set");
+            System.out.println("3. Add to Data Set");
+            System.out.println("4. Create Graph");
+            System.out.println("5. Save Data Set");
+            System.out.println("6. Load Data Set");
+            System.out.println("7. View Saved Data Sets");
+            System.out.println("8. Graph Settings");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
+
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    list[0] = createDataSet();
+
+                    break;
+                case 2:
+                    printDataPoints(list[0]);
+                    break;
+                case 3:
+                    addDataPoints(list[0]);
+                    break;
+                case 4:
+                    new createGraphWindow(list[0]);
+                    break;
+                case 5:
+                    SaveObjectArrayStart(list[0]);
+                    break;
+                case 6:
+                    list[0] = loadDataSet();
+                case 7:
+                    loadSets();
+                    break;
+                case 8:
+                    grOptionsMenu();
+                case 0:
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        } while (choice != 0);
+
+
+    }
+}
